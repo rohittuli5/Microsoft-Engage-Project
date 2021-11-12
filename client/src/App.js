bcc2ee4e516862e51c8edbb67198b3d3f5b84791
Rohit Tuli <rohittuli08@gmail.com>
2021-11-12 19:14:59 +0530

    heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{!data ? "Loading..." : data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-app.listen(PORT, () => {
-console.log(`Server listening on ${PORT}`);
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
